70a1e669e62bdabc55517a76ea5c5631ae63ba0f
Dev-Bruno-Dias <brunodiasgoncalves53@gmail.com>
2026-03-04 12:17:52 -0400

    Aula

---
+++ backend/src/todos/dto/create-todo.dto.ts
@@ -0,0 +1,17 @@
+
+export class CreateTodoDto {
+  title: string;
+  description: string;
+  completed: boolean;
+  priority: TodoPriority;
+  dueAt: Date;
+  completedAt: Date;
+  userId :string;
+  createdAt: Date;
+  updateAt: Date;
+}
+enum TodoPriority{
+    LOW = 'LOW',
+    MEDIUM = 'MEDIUM',
+    HIGH = 'HIGH',
+}
