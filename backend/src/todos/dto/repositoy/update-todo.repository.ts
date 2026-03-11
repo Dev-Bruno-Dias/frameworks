1add87369b0877148a602b09638c65d4585c72aa
Dev-Bruno-Dias <brunodiasgoncalves53@gmail.com>
2026-03-11 12:02:57 -0400

    Delete e update atualizado

--- backend/src/todos/dto/repositoy/update-todo.repository.ts
+++ backend/src/todos/dto/repositoy/update-todo.repository.ts
@@ -1,13 +1,12 @@
 import { Injectable } from "@nestjs/common";
-import { Prisma } from "@prisma/client/extension";
 import { PrismaService } from "src/shared/databases/prisma.database";
-import { CreateTodoDto } from "../create-todo.dto";
+import { UpdateTodoDto } from "../update-todo.dto";
 
 @Injectable()
 export class UpdateTodoByID {
     constructor(private readonly prisma: PrismaService){}
 
-    async update(data: CreateTodoDto){  //parece que está faltando o where id
-        return await this.prisma.todo.update({data}); 
+    async update(id: string, data: UpdateTodoDto){  
+        return await this.prisma.todo.update({where: {id}, data }); 
     }
 }
