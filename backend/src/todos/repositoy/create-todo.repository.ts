82ce1806d6b29cec278b7934fbfb78bc442b6f98
Dev-Bruno-Dias <brunodiasgoncalves53@gmail.com>
2026-04-29 16:14:37 -0400

    Correção pós teste de api

--- backend/src/todos/repositoy/create-todo.repository.ts
+++ backend/src/todos/repositoy/create-todo.repository.ts
@@ -5,12 +5,10 @@ import { CreateTodoDto } from "../dto/create-todo.dto";
 
 @Injectable()
 export class CreateTodoRepository {
-    create(data: CreateTodoDto) {
-        throw new Error("Method not implemented.");
-    }
+
     constructor(private readonly prisma: PrismaService){} //Inicializa o serviço do adaptador 
 
-    async execute(data: CreateTodoDto){ // Cria Dados - baseando do se no que foi tipado no Dto
+    async create(data: CreateTodoDto){ // Cria Dados - baseando do se no que foi tipado no Dto
         return await this.prisma.todo.create({ data }); // passe o parametro para especificar qual os dados serão utilizados
     }
 }
