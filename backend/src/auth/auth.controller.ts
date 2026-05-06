aa6885edc4761168ab644c50a00b5fba5c4f8e4b
Dev-Bruno-Dias <brunodiasgoncalves53@gmail.com>
2026-05-06 16:20:48 -0400

    Começo das validações

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,1 @@
+//Para não ficar vazio
---
+++ backend/src/auth/repository/create-user.repository.ts
@@ -0,0 +1,27 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/shared/databases/prisma.database";
+
+type CreateUserParams = {
+    name?: string;
+    email: string;
+    passwordHash:string;
+};
+
+
+
+
+@Injectable()
+export class CreateUserepository {
+    constructor(private readonly prisma:PrismaService) {}
+
+    async create(data:CreateUserParams){
+        return await this.prisma.user.create({
+            data,
+            select: {
+                id:true,
+                name:true,
+                email:true,
+            }
+        })
+    }
+}
